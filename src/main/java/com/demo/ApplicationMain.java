3c22490e71fd5916694af58e5c3759afdbdc1c98
Prinalchandra.Patel <Scricket@10>
2017-11-09 17:01:18 -0700

    code changes for rabbit listener

--- src/main/java/com/demo/ApplicationMain.java
+++ src/main/java/com/demo/ApplicationMain.java
@@ -1,7 +1,10 @@
 package com.demo;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,6 +21,8 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApplicationMain {
 
@@ -29,12 +34,20 @@ public class ApplicationMain {
     }
 
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    @PostConstruct
+    public RabbitTemplate rabbitTemplate() {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
+        TopicExchange topicExchange = new TopicExchange("testTopic");
+        Queue queue  = new Queue("test");
+        rabbitAdmin.declareExchange(topicExchange);
+        rabbitAdmin.declareQueue(queue);
+        Binding binding = BindingBuilder.bind(queue).to(topicExchange).with("test");
+        System.out.println("Inside methods");
         for(int i=0; i<11; i++){
-            rabbitTemplate.convertAndSend("test", "This is the Data field " +i);
+            System.out.println("inside for");
+            rabbitTemplate.convertAndSend("testTopic","test", "This is the Data field " +i);
         }
        // rabbitTemplate.convertAndSend("test", "This is the Data field");
         return  rabbitTemplate;
@@ -58,11 +71,5 @@ public class ApplicationMain {
 
         return messageConverter;
     }
-    @Bean
-    public Queue queue() {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
-        Queue queue  = new Queue("test");
-        rabbitAdmin.declareQueue(queue);
-        return queue;
-    }
+
 }
